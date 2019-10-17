0ec737c0ca00bf5f35c0f75423edfa478435bab4
Matheus Artur Cabral Costa <macc@ic.ufal.br>
2019-10-16 22:04:36 -0300

    6

--- Payment.java
+++ Payment.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
 
-public class payment {
+public class Payment {
 
     public static Scanner input = new Scanner(System.in);
     static final int size = 100;
     
-	public static void main(String[] args) {
-	String commandString;
+    public static void main(String[] args) {
+	String commandString, placer;
 	int currentDay;
 	
 	String aux;
@@ -22,11 +22,6 @@ public class payment {
 	
 
 	int globalId = 1;
-	
-	String []schedules = new String[20];
-	schedules[0] = "mensal";
-	schedules[1] = "semanal 1 sexta";
-	schedules[2] = "semanal 2 sexta";
 	
 	String [][]employees = new String[size][13];
 		
@@ -117,7 +112,7 @@ public class payment {
 		    
 		}
 		
-		System.out.printf("\tYour newly created employee has ID: %d%n", globalId);
+		System.out.printf("\t%nWARNING: Your newly created employee has ID: %d%n%n", globalId);
 		globalId++;
 
 	    } else if (commandString.equals("edit")) {
@@ -136,33 +131,45 @@ public class payment {
 		aux = input.nextLine();
 		employees[intAux][1] = aux;
 		
-		System.out.print("\tType < salaried | commisioned | hourly >: ");
-		aux = input.nextLine();
-		employees[intAux][2] = aux;
+		while(true)
+		    {
+			System.out.print("\tType < salaried | commisioned | hourly >: ");
+			aux = input.nextLine();
+			employees[intAux][2] = aux;
 		
-		if(aux.equals("salaried")){
-		    System.out.print("\tEnter payment (numbers only): ");
-		    aux = input.nextLine();
-		    employees[intAux][3] = aux;
-		}
-		else if(aux.equals("comissioned")){
-		    System.out.print("\tEnter payment (numbers only): ");
-		    aux = input.nextLine();
-		    employees[intAux][3] = aux;
+			if(aux.equals("salaried")){
+			    System.out.print("\tEnter payment (numbers only): ");
+			    aux = input.nextLine();
+			    employees[intAux][3] = aux;
+			    employees[intAux][4] = "-1";
+			    employees[intAux][5] = "-1";
+			    break;
+			}
+			else if(aux.equals("comissioned")){
+			    System.out.print("\tEnter payment (numbers only): ");
+			    aux = input.nextLine();
+			    employees[intAux][3] = aux;
 
-		    System.out.print("\tEnter percentage (%) on sales (numbers only): ");
-		    aux = input.nextLine();
-		    employees[intAux][4] = aux;
-		}
-		else{
-		    employees[intAux][3] = "-1";
-		    employees[intAux][4] = "-1";
+			    System.out.print("\tEnter percentage (%) on sales (numbers only): ");
+			    aux = input.nextLine();
+			    employees[intAux][4] = aux;
+			    employees[intAux][5] = "-1";
+			    break;
+			}
+			else if(aux.equals("hourly")){
+			    employees[intAux][3] = "-1";
+			    employees[intAux][4] = "-1";
 
-		    System.out.print("\tEnter payment per worked hour (numbers only): ");
-		    aux = input.nextLine();
+			    System.out.print("\tEnter payment per worked hour (numbers only): ");
+			    aux = input.nextLine();
 		    
-		    employees[intAux][5] = aux;
-		}
+			    employees[intAux][5] = aux;
+			    break;
+			}
+			else {
+			    System.out.printf("\tInvalid!%n");
+			}
+		    }
 					
 		System.out.print("\tPayment method < Mail | Bank | by Hand >: ");
 		aux = input.nextLine();
@@ -181,13 +188,67 @@ public class payment {
 		    
 		}
 	    } else if (commandString.equals("remove")) {
+		System.out.println("Please input user ID:");
+		intAux = input.nextInt();
 
-	    } else if (commandString.equals("launch sale")) {
+		employees[intAux][1] = "null";
+		employees[intAux][2] = "null";
+		employees[intAux][3] = "null";
+		employees[intAux][4] = "null";
+		employees[intAux][5] = "null";
+		employees[intAux][6] = "null";
+		employees[intAux][7] = "null";
+		employees[intAux][8] = "null";
+		employees[intAux][9] = "null";
 
-	    } else if (commandString.equals("launch pointcard")) {
+	    }
+	    else if (commandString.equals("launch sale")) {
+		System.out.println("Please input user ID:");
+		intAux = input.nextInt();
 
-	    } else if (commandString.equals("service fee")) {
+		placer = employee[intAux][2];
+		if(placeer.equals("commisioned"))
+		    {
+			System.out.print("\tEnter payment per worked hour (numbers only): ");
+			aux = input.nextLine();
+			employees[intAux][9] = aux;
+		    }
+		else
+		    {
+			System.out.printf("\tInvalid! user is not commisioned%n");
+		    }
+	    }
+	    else if (commandString.equals("launch pointcard")) {
+		System.out.println("Please input user ID:");
+		intAux = input.nextInt();
 
+		placer = employee[intAux][2];
+		if(placeer.equals("hourly"))
+		    {
+			System.out.print("\tEnter worked hours today (numbers only): ");
+			aux = input.nextLine();
+			employees[intAux][10] = aux;
+		    }
+		else
+		    {
+			System.out.printf("\tInvalid! user is not commisioned%n");
+		    }
+	    }
+	    else if (commandString.equals("service fee")) {
+		System.out.println("Please input user ID:");
+		intAux = input.nextInt();
+
+		placer = employee[intAux][7];
+		if(placeer.equals("yes"))
+		    {
+			System.out.print("\tEnter tariff price (numbers only): ");
+			aux = input.nextLine();
+			employees[intAux][11] = aux;
+		    }
+		else
+		    {
+			System.out.printf("\tInvalid! user is not commisioned%n");
+		    }
 	    } else if (commandString.equals("run payroll")) {
 
 	    } else if (commandString.equals("undo")) {
@@ -195,7 +256,18 @@ public class payment {
 	    } else if (commandString.equals("redo")) {
 
 	    } else if (commandString.equals("new agenda")) {
+		System.out.println("Please input user ID:");
+		intAux = input.nextInt();
 
+		System.out.print("\tEnter cicle < monthly | bimonthly | weekly >: ");
+		aux = input.nextLine();
+		employees[intAux][12] = aux;
+
+		System.out.print("\tEnter day, for monthly: 1-31 | for :bimonthly 1-15 | for weekly 1-7: ");
+		aux = input.nextLine();
+		employees[intAux][13] = aux;
+
+		
 	    } else if (commandString.equals("exit")) {
 
 	    } else if (commandString.equals("help") || commandString.equals("?")) {
